c256491eeb1b8384607e3a264507d42e44a92d80
jiawenng <ng.jia.wen@hotmail.com>
2018-06-14 12:02:12 +0800

    Inital Commit

---
+++ functional_tests.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+chromedriver = '/usr/local/bin/chromedriver'
+browser = webdriver.Chrome(chromedriver)
+browser.get('http://localhost:8000')
+assert 'Django' in browser.title
