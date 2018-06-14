7ddea19125a8daa9601dafb02a660d7c57394af4
jiawenng <ng.jia.wen@hotmail.com>
2018-06-14 12:30:42 +0800

    First Functional Test written out in comments, now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,25 @@
 from selenium import webdriver
-chromedriver = '/usr/local/bin/chromedriver'
-browser = webdriver.Chrome(chromedriver)
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        chromedriver = '/usr/local/bin/chromedriver'
+        self.browser = webdriver.Chrome(chromedriver)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
